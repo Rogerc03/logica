bf94b0b26d74a9349bfcd974eb0e9712dd4c4e5b
unknown <rogerc03silva@hotmail.com>
2023-06-29 19:43:31 -0300

    Logica de programação

---
+++ condicao/index.js
@@ -0,0 +1,43 @@
+
+const pessoa = 'Rogerio'
+let idade = 35
+
+if(idade > 18) {
+    console.log('Verdade', idade)
+} else{
+    console.log("Falso", idade)
+
+}
+//Operadores lógicos
+// soma:+
+// Subtrair: -
+// Dividir: /
+// Multiplicar: *
+// Resto: %
+
+let num1 = 0
+let num2 = 6
+
+const resultado = num1 + num2
+//console.log(resultado % 2 == 0)
+//
+
+//if (num1 % 2 === 0) {
+     // console.log('par', num1)
+
+//} else {
+   //  console.log('impar', num1)
+//}
+//Escreva um programa que verifique se um número é positivo, negativo ou zero.
+
+let valor = 0;
+
+if ( valor === 0) {
+    console.log('Zero')
+} else if(valor > 0) {
+    console.log('numero positivo')
+}else {
+    console.log('numero positivo')
+}
+
+
