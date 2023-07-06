a6c5623ebaf3ea2a0b7bd09749149568cb4ed28b
unknown <rogerc03silva@hotmail.com>
2023-07-06 19:42:25 -0300

    exercicios

---
+++ switch/switch-case.js
@@ -0,0 +1,93 @@
+//switch (18) {
+  // case :
+  //   console.log('Maior ou igual a 18')
+ //  break;
+  // default:
+     
+
+  // break;
+
+//}
+
+// switch (8) {
+//     case 1:
+//         console.log('domingo')
+//         break;
+//     case 2:
+//         console.log('Segunda')
+//         break;
+//     case 3:
+//         console.log('Terça')
+//         break;
+//     case 4:
+//         console.log('Quarta')
+//         break;
+//     case 5:
+//        console.log('Quinta')
+//        break;
+//     case 6:
+//        console.log('Sexta')
+//        break;
+//     case 7:
+//       console.log('Sabado')
+//       break;
+//     default:
+//         console.log('Esse dia não existe!')
+        
+// //}
+
+
+// switch (3) {
+//     case 1:
+//         console.log('Janeiro')
+//         break;
+//     case 2:
+//         console.log('Fevereiro')
+//         break;
+//     case 3:
+//         console.log('Março')
+//         break;
+//     case 4:
+//         console.log('Abril')
+//         break;
+//     case 5:
+//        console.log('Maio')
+//        break;
+//     case 6:
+//        console.log('Junho')
+//        break;
+//     case 7:
+//       console.log('julho')
+//       break;
+//     case 8:
+//         console.log('Agosto')
+//         break;
+//     case 9:
+//         console.log('Setembro')
+//         break;
+//     case 10:
+//         console.log('Outubro')
+//         break;
+//     case 11:
+//         console.log('Novembro')
+//         break;
+//     case 12:
+//         console.log('Dezembro')
+//         break;
+//     default:
+//         console.log('Não existe esse mês!')       
+        
+// }
+
+ let vogal = 'b'
+ if (vogal ==='a' || vogal === 'e' || vogal ==='i'|| vogal ==="o" || vogal ==="u"){
+    console.log('vogal')
+
+ }else {
+    console.log('consoantes')
+ }
+ 
+    
+ 
+
+
