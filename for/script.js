d665b4ead7f958da5585a4548f3e048f87667425
unknown <rogerc03silva@hotmail.com>
2023-07-11 19:50:16 -0300

    for

---
+++ for/script.js
@@ -0,0 +1,55 @@
+// for (let i=0; i < 10;i++ ){
+//     console.log(i)
+// }
+
+// for ( let i = 1; i <= 10; i++){
+//     if(i % 2 === 0){
+//         console.log( i,"Número par")
+//     } else {
+//         console.log(i, 'Número impar')
+//     }
+// }
+
+
+//  for (let i = 1; i <= 5; i++){
+//     console.log(i)
+
+//  } 
+
+// for ( let i = 1; i <= 20; i++){
+//     if ( i % 3 === 0){
+//         console.log( i, 'Multiplos de 3')
+//     }
+// }
+
+//     for (let i = 2; i <= 100; i++){
+//         let primo = true;
+//         for ( let j = 2; j < i; j++){
+
+//         }
+// }
+
+// Array
+// const arr = [ 7, 10, 4]
+
+// for ( let i = 0; i < arr.length; i++){
+//     console.log(arr[i], i )
+// }
+
+// const arr = [ 8, 10, 12, 14, 16, 18, 20, 24, 26, 28, 30]
+
+// for ( let i = 0; i < arr.length; i++){
+//     console.log(arr[i]/2)
+// }
+
+
+const arr = [ 10, 20, 30, 40, 50];
+let soma = 0
+for (let i = 0; i < arr.length; i++){
+    soma += arr[i];
+}
+ 
+   console.log(soma/arr.length)
+
+   
+
