46bf70a260c6108ec2cf55b4b28ebbcb905acab4
unknown <rogerc03silva@hotmail.com>
2023-07-27 17:52:43 -0300

    a

---
+++ funcoes/funcoes.js
@@ -0,0 +1,44 @@
+// function soma(){
+//     console.log(4+5)
+// }
+// soma()
+
+// function somaNum (num1, num2){
+//     console.log(num1+num2)
+
+// }
+// somaNum(2, 3)
+
+// Escreva uma função que receba o peso (em quilograma) e a altura (em metros)
+// de uma pessoa  como entrada e retorne o indice de massa corporal (IMC).
+// O IMC é calculado dividindo o peso pela altura ao quadrado.
+// IMC abaixo de 18.5: Abaixo do peso
+// IMC entre 18.5 e 24.9 : peso normal
+// IMC entre 25 e 29.9: Sobrepeso
+// IMC entre 30 e 34.9: Obesidade Garau I
+// IMC entre 35 e 39.9: Obesidade Garau II
+// IMC acima de 40: Obesidade Garau III (Obesidade Mórbida)
+// IMC = peso / altura^2
+// Arredonde o valor do IMC para duas casas decimais utilizando a
+// funçãode arredondamento da linguagem de programção que estiver utilizando
+
+
+// function calculoImc(peso, altura) {
+  
+//        const imc = (peso/(altura**2)).toFixed(2)
+
+//        if (imc < 18.5) {
+//            console.log(imc,"abaixo do peso")        
+//        } else if (imc >= 18.5 && imc <= 24.9)
+//          console.log(imc,"Peso normal")
+//     }
+//   calculoImc(120,2.8)
+
+// Receber três parametros : Dois números
+// e uma operação matématica representada por uma string.
+// Operação: soma, subtrção, multiplicação e divisão
+
+
+
+
+
