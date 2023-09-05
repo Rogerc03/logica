2c87506a7619570ec9f929798cc2a277e0604af1
Rogerc03 <rogerc03silva@hotmail.com>
2023-09-05 12:09:05 -0300

    add

--- geradorDeSenha/img/js/script.js
+++ geradorDeSenha/img/js/script.js
@@ -1,6 +1,6 @@
 //Seleção de Elementos
 const generatePasswordButton = document.querySelector("#generate-password")
-const generatedPasswordButton = document.querySelector("#generated-password")
+const generatedPasswordElement = document.querySelector("#generated-password")
 
 //Funcões
 //Letras, Números e símbolos
@@ -22,9 +22,36 @@ const getLetterUpperCase = () => {
  };
  
  
-console.log(getSymbol());
+ const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+     let password = "";
+     const passwordLength = 10;
+     const generators = [
+          getLetterLowerCase,
+          getLetterUpperCase,
+          getNumber,
+          getSymbol
+     ];
+
+     for (i = 0; i < passwordLength; i = i + 4) {
+        generators.forEach(() => {
+         const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+
+         password += randomValue;
+
+        });
+      
+     };
+      password = password.slice(0, passwordLength);
+
+      generatedPasswordElement.style.display = "block";
+      generatedPasswordElement.querySelector("h4").innerText = password;
+ };
 
 //Eventos
 generatePasswordButton.addEventListener("click", () =>{
-    console.log("teste")
+   generatePassword(
+      getLetterLowerCase,
+      getLetterUpperCase,
+      getNumber,
+      getSymbol);
 });
