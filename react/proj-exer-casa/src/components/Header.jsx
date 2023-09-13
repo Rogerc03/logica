e54f66ad7784f2c513bc9ab938bdd47ba7d88bc8
Rogerc03 <rogerc03silva@hotmail.com>
2023-09-13 12:56:10 -0300

    add

---
+++ react/proj-exer-casa/src/components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+    return <nav className="w-36 h-9 flex flex-col items-center border-b-2">
+        <div>
+            <img src="https://leao100.com.br/wp-content/uploads/2022/02/logo-leao100.png" alt="Logo leao 100" />
+        </div>
+        <div>
+            <ul>
+                <li>Início</li>
+                <li>Sobre</li>
+                <li>Categorias</li>
+                <li>Promoção</li>
+                <li>Minha conta</li>
+                <li>R$</li>
+            </ul>
+        </div>
+    </nav>
+}
