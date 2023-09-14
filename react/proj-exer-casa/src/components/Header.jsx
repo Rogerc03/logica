c247338621e1fb384f17fa1e31e17ab5b73db474
Rogerc03 <rogerc03silva@hotmail.com>
2023-09-13 22:24:39 -0300

    .

--- react/proj-exer-casa/src/components/Header.jsx
+++ react/proj-exer-casa/src/components/Header.jsx
@@ -1,16 +1,21 @@
 export default function Header() {
-    return <nav className="w-36 h-9 flex flex-col items-center border-b-2">
-        <div>
-            <img src="https://leao100.com.br/wp-content/uploads/2022/02/logo-leao100.png" alt="Logo leao 100" />
+    return <nav className="fixed w-(1349px) mt-0 mb-0 top-0">
+        <div className="h-3 w-20">
+            <img width="800px" height="697" src="https://leao100.com.br/wp-content/uploads/2022/02/logo-leao100.png" alt="Logo leao 100" />
         </div>
         <div>
             <ul>
-                <li>Início</li>
-                <li>Sobre</li>
-                <li>Categorias</li>
-                <li>Promoção</li>
-                <li>Minha conta</li>
-                <li>R$</li>
+                <li><a href="">Início</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href=""><span><i></i></span>Categorias</a><ul><li><a href="">Copo</a></li>
+                    <li><a href="">Relançamento</a></li>
+                    <li><a href="">Pop</a></li>
+                    <li><a href="">Les Bleus</a></li>
+                    <li><a href="">Réveillon</a></li>
+                    <li><a href="">Serviços</a></li></ul></li>
+                <li><a href="">Promoção</a></li>
+                <li><a href="">Minha conta</a></li>
+                <li><a href="">R$</a></li>
             </ul>
         </div>
     </nav>
